60d1bf0044963fc8623df610337816e099005a30
Cesar Gomez <cesargomez@tangosource.com>
2013-05-25 18:35:06 -0500

    spree rails project created with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '57cc956ed84ae54bc83c0dab07031f196f19fe17cdc6de237865229ad19d6932dfa714ef44dd5ceb7ef0a8afbfa6fc8cdd26b7d48ef475e14eadc9eade432e95'
